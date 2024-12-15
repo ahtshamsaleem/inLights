2c7106fffde0ad32daa3673a8548ca800172c507
Ihtisham Ul Haq <ahtshamsaleem51@gmail.com>
2024-12-15 20:48:05 +0500

    middleware update

--- middleware.js
+++ middleware.js
@@ -1,33 +1,24 @@
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
-import { connectDB } from "./lib/connectDB";
-
 
 export async function middleware(request) {
+  const token = request.cookies.get("token");
 
-  
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
-	const token = request.cookies.get("token");
+  const secret = new TextEncoder().encode(process.env.SECRET_JWT);
+  const isTokenValid = await jwtVerify(token.value, secret);
 
-    
-	if (!token) {
-		return NextResponse.redirect(new URL("/login", request.url));
-	}
-
-
-	const secret = new TextEncoder().encode(process.env.SECRET_JWT);
-	const isTokenValid = await jwtVerify(token.value, secret);
-
-
-	if (!isTokenValid) {
-		return NextResponse.redirect(new URL("/login", request.url));
-	}
+  if (!isTokenValid) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
-
 
 // See "Matching Paths" below to learn more
 export const config = {
-	matcher: "/ig-posts/:path*",
+  matcher: "/ig-posts/:path*",
 };
 
 //
